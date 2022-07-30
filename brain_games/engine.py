8f2bd80cacb15b0caa8b9350cc34c25870c3b4e2
Александр Матвеев <aleksandrmatveev@MacBook-Pro-Aleksandr.local>
2022-07-30 12:08:00 +0300

    refactoring game engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,9 +1,13 @@
+"""The game engine."""
+import prompt
 
-
-def game_engine(calculate, head, greet):
-    user_name = greet()
-    head()
-    for i in range(0, 3):
+def game_engine(calculate, QUESTION):
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print(QUESTION)
+    STEP_COUNT = 3
+    for _ in range(STEP_COUNT):
         correct_answer = calculate()
         answer = input('Your answer: ')
         if correct_answer == answer:
@@ -13,6 +17,5 @@ def game_engine(calculate, head, greet):
                 f"'{answer}' is wrong answer ;(."
                 f"Correct answer was '{correct_answer}'.\n"
                 f"Let's try again, {user_name}!")
-            break
-        if i == 2:
-            print(f"Congratulations, {user_name}!")
+            return
+    print(f"Congratulations, {user_name}!")
