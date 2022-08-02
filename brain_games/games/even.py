c733e710d85e544bed20c7e755167756b6e7530d
Александр Матвеев <aleksandrmatveev@dial-up.86.57.212.78.grodno.by>
2022-08-02 11:23:37 +0300

    refactoring even game logic

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,9 +5,6 @@ QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def calculate():
-    random_number = randint(1, 100)
-    print(f"Question: {random_number}")
-    if random_number % 2 == 0:
-        return "yes"
-    else:
-        return "no"
+    task = randint(0, 100)
+    correct_answer = 'no' if task % 2 else 'yes'
+    return task, correct_answer
